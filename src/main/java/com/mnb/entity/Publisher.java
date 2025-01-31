245e09e371730ff2af3f53dc458d53229b5db750
Rushabh Shah <rushabh725@gmail.com>
2025-01-31 12:45:24 +0530

    updated spring boot version and new template changes for new version

--- src/main/java/com/mnb/entity/Publisher.java
+++ src/main/java/com/mnb/entity/Publisher.java
@@ -17,6 +17,25 @@ public class Publisher {
     Integer id;
     @Column(name = "PUBLISHER_NAME")
     String publisherName;
-    @Column(name = "DESCRIPTION")
+    
+    public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getPublisherName() {
+		return publisherName;
+	}
+	public void setPublisherName(String publisherName) {
+		this.publisherName = publisherName;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	@Column(name = "DESCRIPTION")
     String description;
 }
--- src/main/java/com/mnb/service/BookServiceImpl.java
+++ src/main/java/com/mnb/service/BookServiceImpl.java
@@ -42,6 +42,6 @@ public class BookServiceImpl implements BookService{
 
     @Override
     public List<Book> findBookByName(String keyword) {
-        return bookRepository.findByName(keyword);
+        return bookRepository.findByBookName(keyword);
     }
 }
